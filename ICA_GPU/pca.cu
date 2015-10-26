93bf811fb5d009241ebeb924785410cf2924532c
przemek1899 <przemek1916@wp.pl>
2015-10-26 15:20:23 +0100

    set device

--- ICA_GPU/pca.cu
+++ ICA_GPU/pca.cu
@@ -8,9 +8,22 @@
 #include "helper_cuda.h"
 #include "device_launch_parameters.h"
 
+__global__ void pca_gpu(){
+
+}
+
 void runPCA(void){
 
-	//checkCudaErrors(cudaXxxx(...));
+	checkCudaErrors(cudaSetDevice(0));
+	/*
+	float *dev_A, *dev_C;
+	//allocate memory
+    checkCudaErrors(cudaMalloc(&dev_A, m*n*sizeof(float)));
+    checkCudaErrors(cudaMalloc(&dev_C, m*m*sizeof(float)));
+
+	// copy data from cpu to gpu memory
+    checkCudaErrors(cudaMemcpy(dev_A, A, m*n*sizeof(float), cudaMemcpyHostToDevice));
+	*/
 
 	cudaEvent_t start, stop;
 	float elapsedTime;
@@ -27,6 +40,18 @@ void runPCA(void){
 	checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
 	checkCudaErrors(cudaEventDestroy(start));
 	checkCudaErrors(cudaEventDestroy(stop));
+
+
+	/*
+	//copy results from gpu memory to cpu
+	checkCudaErrors(cudaMemcpy(C, dev_C, m*m*sizeof(float), cudaMemcpyDeviceToHost));
+
+	//free gpu memory
+    checkCudaErrors(cudaFree(dev_C));
+    checkCudaErrors(cudaFree(dev_A));
+    */
+
+	checkCudaErrors(cudaDeviceReset()); // dla debuggera
 
 	printf("Kernel-only time: %f ms\n", elapsedTime);
 
