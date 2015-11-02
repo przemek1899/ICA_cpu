7ea3c951cb8270a5dc40d023d4c4bcdff2c1eded
przemek1899 <przemek1916@wp.pl>
2015-11-02 20:23:08 +0100

    zakomentowanie na potrzebe kompilacji

--- PCA_GPU/main.cpp
+++ PCA_GPU/main.cpp
@@ -1,17 +1,17 @@
-#include "helper_timer.h"
+//#include "helper_timer.h"
 #include "pca.cuh"
 
 int main(){
-
+	/*
 	StopWatchInterface *timer = NULL;
 	sdkCreateTimer(&timer);
-	sdkStartTimer(&timer);
+	sdkStartTimer(&timer);*/
 	
 	runPCA();
 
-	sdkStopTimer(&timer);
-	printf("Processing time: %f ms\n", sdkGetTimerValue(&timer));
-	sdkDeleteTimer(&timer);
+	//sdkStopTimer(&timer);
+	//printf("Processing time: %f ms\n", sdkGetTimerValue(&timer));
+	//sdkDeleteTimer(&timer);
 
 	return 0;
 }
