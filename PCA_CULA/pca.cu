83a2b100e5bce5f481d8a54d61ac7cf624accac1
przemek1899 <przemek1916@wp.pl>
2015-11-17 15:29:55 +0100

    czytanie macierzy A_mu

--- PCA_CULA/pca.cu
+++ PCA_CULA/pca.cu
@@ -285,7 +285,6 @@ void runPCA(nifti_data_type * A, int m, int n){
 	checkCudaErrors(cudaEventRecord(start, 0));
 	
 	get_mu<<<numBlocks, threadsPerBlock, shared_mem_size>>>(AT_dev, n, m, iter, MU_dev);
-	//get_mu<<<numBlocks, threadsPerBlock, shared_mem_size>>>(A_dev, m, n, iter);
 	checkCudaErrors(cudaDeviceSynchronize());
 	checkCudaErrors(cudaGetLastError());
 
@@ -298,7 +297,10 @@ void runPCA(nifti_data_type * A, int m, int n){
 	status = culaDeviceSgeTranspose(n,  m, AT_dev, n, A_dev, m);
     checkStatus(status);
 
-	printf("Calculete mu-only time: %f ms\n", elapsedTime);
+	printf("Calculate mu-only time: %f ms\n", elapsedTime);
+	
+	checkCudaErrors(cudaMemcpy(A, A_dev, m*20*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
+	print_matrix_data(A, m, 20, 0, 1, "A_mu.txt");
 	
 	//sprawdzenie wartoœci - kopiowanie do cpu - to w przyszlosci zostanie usuniête
 	nifti_data_type *MU = (nifti_data_type*) malloc(max*sizeof(nifti_data_type));
@@ -328,9 +330,6 @@ void runPCA(nifti_data_type * A, int m, int n){
     status = culaDeviceSgesvd(jobu, jobvt, m, n, A_dev, lda, S_dev, U_dev, ldu, VT_dev, ldvt);
     checkStatus(status);
 	int new_cols = 20;
-
-	checkCudaErrors(cudaMemcpy(A, AT_dev, m*20*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
-	print_matrix_data(A, m, 20, 0, 1, "gesvd.txt");
 
 	threadsPerBlock = 512;
 	int blocks_per_column = getRound(m, threadsPerBlock) / threadsPerBlock;
