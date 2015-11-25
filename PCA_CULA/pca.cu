7088d611516f75473f7be6ddc4770391e0498936
przemek1899 <przemek1916@wp.pl>
2015-11-25 12:36:49 +0100

    mu shuffle

--- PCA_CULA/pca.cu
+++ PCA_CULA/pca.cu
@@ -13,7 +13,18 @@
 
 #define imin(X, Y)  ((X) < (Y) ? (X) : (Y))
 #define imax(X, Y)  ((X) > (Y) ? (X) : (Y))
+#define NUM_COMPONENTS = 20;
 
+void checkStatus(culaStatus status)
+{
+    char buf[256];
+    if(!status)
+        return;
+    culaGetErrorInfoString(status, culaGetErrorInfo(), buf, sizeof(buf));
+    printf("%s\n", buf);
+    culaShutdown();
+    exit(EXIT_FAILURE);
+}
 
 int getRound(int m, int n){
 
@@ -156,18 +167,40 @@ void print_matrix_data(float * Matrix, int m, int n, int print_to_shell, int wri
 	return;
 }
 
-__device__ __inline__ double shfl_double(double x, int laneId){
+__device__ inline double __shfl_down_double(double var, unsigned int srcLane, int width=32) {
+  int2 a = *reinterpret_cast<int2*>(&var);
+  a.x = __shfl_down(a.x, srcLane, width);
+  a.y = __shfl_down(a.y, srcLane, width);
+  return *reinterpret_cast<double*>(&a);
+}
+
+__device__ inline double __sfhl_down_asm_double(double var, unsigned int delta, int width=32){
+
+	int lo, hi;
+	asm volatile("mov.b64 {%0, %1}, %2;" : "=r"(lo), "=r" (hi) : "d"(var));
+
+	// shuffle the 32b registers
+	lo = __shfl_down(lo, delta, width);
+	hi = __shfl_down(hi, delta, width);
+
+	// recreate the 64b number
+	asm volatile("mov.b64 %0, {%1, %2};" : "=d"(x) : "r"(lo), "r"(hi));
+
+	return var;
+}
+
+__device__ __inline__ double shfl_asm_double(double x, int laneId){
 
 	// Split the double number into 2 32b registers
 	int lo, hi;
-	//asm volatile("mov.b32 {%0, %1}, %2;" : "=r"(lo), "=r" (hi) : "d"(x));
+	asm volatile("mov.b64 {%0, %1}, %2;" : "=r"(lo), "=r" (hi) : "d"(x));
 
 	// shuffle the 32b registers
 	lo = __shfl(lo, laneId);
 	hi = __shfl(hi, laneId);
 
 	// recreate the 64b number
-	//asm volatile("mov.b64 %0, {%1, %2};" : "=d"(x) : "r"(lo), "r"(hi));
+	asm volatile("mov.b64 %0, {%1, %2};" : "=d"(x) : "r"(lo), "r"(hi));
 
 	return x;
 }
@@ -175,48 +208,94 @@ __device__ __inline__ double shfl_double(double x, int laneId){
 __inline__ __device__ double warpDoubleReduce(double val){
 	
 	for (int offset = warpSize/2; offset > 0; offset /= 2){
-		val += shfl_double(val, offset);
+		val += __shfl_down_double(val, offset);
 	}
 }
 
-__global__ void mu_shuffle(nifti_data_type * A, int m, int n, int iter){
+__inline__ __device__ nifti_data_type blockReduceMuColumn(nifti_data_type val){
 	
-	/*
-	every block has an array (shared memory) of ceil((threads_per_blocks / warpSize)) elements
-	*/
-	extern __shared__ nifti_data_type Ash[];
+  static __shared__ nifti_data_type shared[32]; // Shared mem for 32 partial sums
+  int lane = threadIdx.x % warpSize;
+  int wid = threadIdx.x / warpSize;
+
+  val = warpDoubleReduce(val);
+
+  if (lane==0) shared[wid]=val;
+  __syncthreads();
+
+  //read from shared memory only if that warp existed
+  val = (threadIdx.x < blockDim.x / warpSize) ? shared[lane] : 0;
+  if (wid==0) val = warpDoubleReduce(val); //Final reduce within first warp
+
+  return val;
+}
+
+__global__ void mu_shuffle(nifti_data_type * A, int m, int n, int iter, nifti_data_type* MU){
+
+	// na razie zakladamy, ze jeden blok mo¿e przykryæ ca³¹ kolumnê, czyli liczba elementów w kolumnie <= 1024
+
+	static __shared__ nifti_data_type shared[32]; // Shared mem for 32 partial sums
+	int lane = threadIdx.x % warpSize;
+	int wid = threadIdx.x / warpSize;
+
 	int tid = threadIdx.x;
 	int difference = blockDim.x - m;
-	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	int warpId = threadIdx.x / warpSize;
+	int grid_index = blockIdx.x * blockDim.x + threadIdx.x;
 
-	//#pragma unroll
 	for (int i=0; i<iter; i++){
 		int columnIndex = blockIdx.x + i * gridDim.x;
-		int globalDataIndex = index - blockIdx.x * difference + i * gridDim.x * m;
+		int globalDataIndex = grid_index - blockIdx.x * difference + i * gridDim.x * m;
 		
 		if (columnIndex < n){
-			
 			// do reduction by shuffle instructions in warps
 			nifti_data_type val = 0.0;
 			if (tid < m){
 				val = A[globalDataIndex];
 			}
-			int lane = threadIdx.x % warpSize;
 			val = warpDoubleReduce(val);
+			if (lane==0) shared[wid]=val;
+			__syncthreads(); 
+			
+			//read from shared memory only if that warp existed
+			val = (threadIdx.x < blockDim.x / warpSize) ? shared[lane] : 0;
 
-			if(lane == 0) Ash[warpId] = val;
-			__syncthreads();
+			if (wid==0) val = warpDoubleReduce(val); //Final reduce within first warp
 
-			//the last part of reduction TODO
+			nifti_data_type mean = val / m;
+			if (tid < m){
+				A[globalDataIndex] -= mean;
+			}
+			if (tid == 0 ){
+				MU[columnIndex] = mean;
+			}
 		}
 	}
 }
 
+/*	MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU MU
+
+	w tej wersji funkcji get_mu oblicza tablice jak ponizej:
+
+						M = 163840 (to jest problem - du¿a liczba kolumn)
+		_________________________________________________________
+		|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
+		|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
+  N=121 |___|___|___|___|___|___|___|___|___|___|___|___|___|___|
+		|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
+		|___|___|___|___|___|___|___|___|___|___|___|___|___|___|
+
+		Ka¿dy BLOK wykonuje redukcjê sumy dla JEDNEJ KOLUMNY
+
+		PROBLEM: liczba kolumn jest zbyt du¿y aby "przykryæ" ca³¹ tablicê blokami za jednym razem - dlatego potrzebne s¹ iteracje
+		ALE: co jeœli liczba wierszy (bêdzie wiêksza od 128 albo np. 1024 ??) - to do przemyœlenia
+
+*/
+
 __global__ void get_mu(nifti_data_type * A, int m, int n, int iter, nifti_data_type* MU){
 
 	// tutaj jest zalozenie z m < n (np. m=121, n=163840)
-	// in this version thera are not yet weights, not needed now
+	// every block has an array (shared memory) of ceil((threads_per_blocks / warpSize)) elements
+	
 	extern __shared__ nifti_data_type Ash[];
 	int tid = threadIdx.x;
 	int difference = blockDim.x - m;
@@ -256,19 +335,6 @@ __global__ void get_mu(nifti_data_type * A, int m, int n, int iter, nifti_data_t
 	
 }
 
-void checkStatus(culaStatus status)
-{
-    char buf[256];
-    if(!status)
-        return;
-    culaGetErrorInfoString(status, culaGetErrorInfo(), buf, sizeof(buf));
-    printf("%s\n", buf);
-    culaShutdown();
-    exit(EXIT_FAILURE);
-}
-
-#define NUM_COMPONENTS = 20;
-
 void runPCA(nifti_data_type * A, int m, int n){
 
 	// int DOF = n - 1;
@@ -288,72 +354,24 @@ void runPCA(nifti_data_type * A, int m, int n){
 		jobu = 'N';
 		jobvt = 'O';
 	}
-
+	
+	const int NCOMPONENTS = 20;
 	int lda = m;
     int ldu = m;
     int ldvt = n;
 	int min = imin(m,n);
 	int max = imax(m,n);
+	cudaEvent_t start, stop;
+	float elapsedTime;
 
 	nifti_data_type *S, *U, *VT;
 	nifti_data_type *A_dev, *AT_dev, *MU_dev, *S_dev, *U_dev, *VT_dev;
 
+	// allocation of memory
 	checkCudaErrors(cudaMalloc(&A_dev, m*n*sizeof(nifti_data_type)));
-	checkCudaErrors(cudaMemcpy(A_dev, A, m*n*sizeof(nifti_data_type), cudaMemcpyHostToDevice));
-
-	cudaEvent_t start, stop;
-	float elapsedTime;
-
-	// transpozycja macierzy A w celu obliczenia mu
-	
-	checkCudaErrors(cudaMalloc(&AT_dev, m*n*sizeof(nifti_data_type)));
-	status = culaDeviceSgeTranspose(m, n, A_dev, m, AT_dev, n);
-    checkStatus(status);
-
-	//printf("Calculete transpose-only time: %f ms\n", elapsedTime);
-	
-
-	/* obliczanie wartoœci mu */
-	checkCudaErrors(cudaMalloc(&MU_dev, m*sizeof(nifti_data_type)));
-
-	int shared_mem_size = getRound(min, 32)*sizeof(nifti_data_type);
-	int threadsPerBlock = 128;
-	int numBlocks = 65535;
-	int iter = getRound(m, numBlocks) / numBlocks;
-	printf("shared mem size %d, iter %d\n", shared_mem_size, iter);
-
-	checkCudaErrors(cudaEventCreate(&start));
-	checkCudaErrors(cudaEventCreate(&stop));
-	checkCudaErrors(cudaEventRecord(start, 0));
-	
-	get_mu<<<numBlocks, threadsPerBlock, shared_mem_size>>>(AT_dev, n, m, iter, MU_dev);
-	checkCudaErrors(cudaDeviceSynchronize());
-	checkCudaErrors(cudaGetLastError());
-
-	checkCudaErrors(cudaEventRecord(stop, 0));
-	checkCudaErrors(cudaEventSynchronize(stop));
-	checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
-
-	// transpozycja macierzy AT po obliczenia mu
-	
-	status = culaDeviceSgeTranspose(n,  m, AT_dev, n, A_dev, m);
-    checkStatus(status);
-
-	printf("Calculate mu-only time: %f ms\n", elapsedTime);
-	
-	//checkCudaErrors(cudaMemcpy(A, A_dev, m*20*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
-	//print_matrix_data(A, m, 20, 0, 1, "A_tt.txt");
-	
-	//sprawdzenie wartoœci - kopiowanie do cpu - to w przyszlosci zostanie usuniête
-	// nifti_data_type *MU = (nifti_data_type*) malloc(max*sizeof(nifti_data_type));
-	//checkCudaErrors(cudaMemcpy(MU, MU_dev, max*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
-	
-	// reading & writing mu array - 
-	// print_matrix_data(MU, 1, m, 0, 1, "mu_transpose_file.txt");
-
-	// free(MU);
-	/* koniec obliczania wartoœci mu */
-
+	checkCudaErrors(cudaMalloc(&AT_dev, m*n*sizeof(nifti_data_type))); // AT do transpozycji macierzy
+	checkCudaErrors(cudaMalloc(&MU_dev, m*sizeof(nifti_data_type))); // an array only for check the results, need to be removed in final version
+		
 	S = (nifti_data_type*) malloc(min * sizeof(nifti_data_type));
 	checkCudaErrors(cudaMalloc(&S_dev, min * sizeof(nifti_data_type)));
 
@@ -366,28 +384,63 @@ void runPCA(nifti_data_type * A, int m, int n){
 		checkCudaErrors(cudaMalloc(&VT_dev, ldvt*n*sizeof(nifti_data_type)));
 	}
 
-	/* Perform singular value decomposition CULA */
-	/* coeff = U_dev (m x min)  */
 
+	// copy data from host to device
+	checkCudaErrors(cudaMemcpy(A_dev, A, m*n*sizeof(nifti_data_type), cudaMemcpyHostToDevice));
+
+	// transpozycja macierzy A w celu obliczenia mu
+	status = culaDeviceSgeTranspose(m, n, A_dev, m, AT_dev, n);
+    checkStatus(status);
+
+	//printf("Calculete transpose-only time: %f ms\n", elapsedTime);
+	
+	// ---------- MU calculations -----------------------
+	int shared_mem_size = getRound(min, 32)*sizeof(nifti_data_type);
+	int threadsPerBlock = 128;
+	int numBlocks = 65535;
+	int iter = getRound(m, numBlocks) / numBlocks;
+	printf("shared mem size %d, iter %d\n", shared_mem_size, iter);
+
+	checkCudaErrors(cudaEventCreate(&start));	checkCudaErrors(cudaEventCreate(&stop));	checkCudaErrors(cudaEventRecord(start, 0));
+	
+	//get_mu<<<numBlocks, threadsPerBlock, shared_mem_size>>>(AT_dev, n, m, iter, MU_dev);
+	mu_shuffle<<<numBlocks, threadsPerBlock>>>(AT_dev, n, m, iter, MU_dev);
+	checkCudaErrors(cudaDeviceSynchronize()); checkCudaErrors(cudaGetLastError());
+
+	checkCudaErrors(cudaEventRecord(stop, 0));	checkCudaErrors(cudaEventSynchronize(stop));	
+	checkCudaErrors(cudaEventElapsedTime(&elapsedTime, start, stop));
+
+	// transpozycja macierzy AT po obliczenia mu
+	status = culaDeviceSgeTranspose(n,  m, AT_dev, n, A_dev, m);
+    checkStatus(status);
+
+	printf("Calculate mu-only time: %f ms\n", elapsedTime);
+
+	nifti_data_type *MU = (nifti_data_type*) malloc(max*sizeof(nifti_data_type));
+	checkCudaErrors(cudaMemcpy(MU, MU_dev, max*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
+	print_matrix_data(MU, 1, m, 0, 1, "mu_shuffle.txt");
+	free(MU);
+
+	// ------------- SVD -----------------------------
+	// coeff = U_dev (m x min)
     status = culaDeviceSgesvd(jobu, jobvt, m, n, A_dev, lda, S_dev, U_dev, ldu, VT_dev, ldvt);
     checkStatus(status);
-	int new_cols = 20;
 
 	//checkCudaErrors(cudaMemcpy(S, S_dev, min*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
 	//print_matrix_data(S, min, 0, 0, 1, "S_matrix.txt");
 
+	// -------------- colsign ---------------------------------
 	threadsPerBlock = 512;
 	int blocks_per_column = getRound(m, threadsPerBlock) / threadsPerBlock;
-	int grid_x = getRound(new_cols, 32);
+	int grid_x = getRound(NCOMPONENTS, 32);
 	dim3 grid(blocks_per_column, grid_x);
-
 	shared_mem_size = threadsPerBlock*sizeof(nifti_data_type);
 	printf("shared mem size %d, iter %d\n", shared_mem_size);
 
 	nifti_data_type * intermediate_results;
-	checkCudaErrors(cudaMalloc(&intermediate_results, blocks_per_column*new_cols*sizeof(nifti_data_type)));
+	checkCudaErrors(cudaMalloc(&intermediate_results, blocks_per_column*NCOMPONENTS*sizeof(nifti_data_type)));
 
-	colsign2<<<grid, threadsPerBlock, shared_mem_size>>>(A_dev, m, new_cols, intermediate_results, blocks_per_column, new_cols);
+	colsign2<<<grid, threadsPerBlock, shared_mem_size>>>(A_dev, m, NCOMPONENTS, intermediate_results, blocks_per_column, NCOMPONENTS);
 	checkCudaErrors(cudaDeviceSynchronize());
 	checkCudaErrors(cudaGetLastError());
 
@@ -396,29 +449,28 @@ void runPCA(nifti_data_type * A, int m, int n){
 	//print_matrix_data(intermediate_results_h, blocks_per_column, new_cols, 0, 1, "intermed_res.txt");
 	//free(intermediate_results_h);
 
-	nifti_data_type* maxFindResults = (nifti_data_type*) malloc(new_cols*sizeof(nifti_data_type));
+	// ------------------- get_colsign ----------------------------------------------------
+	nifti_data_type* maxFindResults = (nifti_data_type*) malloc(NCOMPONENTS*sizeof(nifti_data_type));
 	nifti_data_type* maxFindResults_d;
-	checkCudaErrors(cudaMalloc(&maxFindResults_d, new_cols*sizeof(nifti_data_type)));
+	checkCudaErrors(cudaMalloc(&maxFindResults_d, NCOMPONENTS*sizeof(nifti_data_type)));
 
 	dim3 grid2(grid_x, 1);
 	shared_mem_size = blocks_per_column*sizeof(nifti_data_type);
-	get_colsign<<<grid2, blocks_per_column, shared_mem_size>>>(intermediate_results, blocks_per_column, new_cols, A_dev, m, new_cols, maxFindResults_d);
+	get_colsign<<<grid2, blocks_per_column, shared_mem_size>>>(intermediate_results, blocks_per_column, NCOMPONENTS, A_dev, m, NCOMPONENTS, maxFindResults_d);
+	
 	checkCudaErrors(cudaDeviceSynchronize());
 	checkCudaErrors(cudaGetLastError());
 
-	nifti_data_type* coeff = (nifti_data_type*) malloc(m*new_cols*sizeof(nifti_data_type));
-	checkCudaErrors(cudaMemcpy(coeff, A_dev, m*new_cols*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
-	print_matrix_data(coeff, m, new_cols, 0, 1, "coeff_mat.txt");
+	nifti_data_type* coeff = (nifti_data_type*) malloc(m*NCOMPONENTS*sizeof(nifti_data_type));
+	checkCudaErrors(cudaMemcpy(coeff, A_dev, m*NCOMPONENTS*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
+	print_matrix_data(coeff, m, NCOMPONENTS, 0, 1, "coeff_mat.txt");
 
-	checkCudaErrors(cudaMemcpy(maxFindResults, maxFindResults_d, new_cols*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(maxFindResults, maxFindResults_d, NCOMPONENTS*sizeof(nifti_data_type), cudaMemcpyDeviceToHost));
 	checkCudaErrors(cudaFree(maxFindResults_d));
-	print_matrix_data(maxFindResults, new_cols, 0, 0, 1, "max_results.txt");
+	print_matrix_data(maxFindResults, NCOMPONENTS, 0, 0, 1, "max_results.txt");
 	free(maxFindResults);
 
-	checkCudaErrors(cudaEventDestroy(start));
-	checkCudaErrors(cudaEventDestroy(stop));
-
-	//free memory
+	// free memory
 	free(S);
 	free(coeff);
 	checkCudaErrors(cudaFree(intermediate_results));
@@ -435,6 +487,9 @@ void runPCA(nifti_data_type * A, int m, int n){
 		free(VT);
 		checkCudaErrors(cudaFree(VT_dev));
 	}
+
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
 
 	//checkCudaErrors(cudaDeviceReset()); // dla debuggera
 	return;
